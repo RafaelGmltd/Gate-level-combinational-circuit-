46d03190226828b2b4bd96368ca3886566950619
Rafael <gmlt.rafael@gmail.com>
2025-03-05 23:12:00 -0300

    Create behavioral_ level one_bit_adder.v

---
+++ behavioral_ level one_bit_adder.v
@@ -0,0 +1,21 @@
+module add_1
+	(input cin, a, b,       // input ports
+    output reg cout, sum    // output ports
+    );
+	
+always@(cin, a, b)
+	if(a & b | cin & a | cin & b) 
+	cout = 1'b1;
+	else 
+	cout = 0'b0;
+	sum = a ^ b ^cin;
+	
+endmodule
+
+/*
+A more abstract description where implementation details 
+are hidden, and only the system's output behavior is defined.
+
+Note that the carry function will be equal to 1 
+if at least two ones appear at the inputs.
+*/
