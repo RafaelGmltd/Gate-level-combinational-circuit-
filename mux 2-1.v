ad341bef714165f54df7b9ca9677122a7e54b1f7
Rafael <gmlt.rafael@gmail.com>
2025-03-07 11:43:55 -0300

    Create mux 2-1.v

---
+++ mux 2-1.v
@@ -0,0 +1,55 @@
+//Structural Description
+//A simple combinational logic implementation using a ternary operator
+
+module two_one (
+    input wire a, b, sel,  // I/O 
+    output wire y          
+);
+    assign y = sel ? b : a; 
+endmodule
+
+/*
+
+Explicit behavior description using case
+
+module mux2to1_case (
+    input wire a, b, sel, 
+    output reg y
+);
+    always @* begin
+    case (sel)
+    1'b0: y = a;
+    1'b1: y = b;
+    endcase
+    end
+endmodule
+
+Behavioral description using if-else
+
+module mux2to1_if (
+    input wire a, b, sel, 
+    output reg y
+);
+    always @* begin
+    if (sel)
+    y = b;
+    else
+    y = a;
+    end
+endmodule
+
+The multiplexer selects between a and b based on the value of sel
+We can write the Boolean expression
+y = (a & ~sel) | (b & sel)
+This is a minimized SOP (Sum of Products) form, which can easily be derived using the K-map
+
+module mux2to1_logic (
+    input wire a, b, sel, 
+    output wire y
+);
+    assign y = (a & ~sel) | (b & sel);
+endmodule
+
+*/
+
+
