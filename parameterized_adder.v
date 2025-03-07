a0aef8909705aefb5e68ba60aabe122d9b593f4d
Rafael <gmlt.rafael@gmail.com>
2025-03-06 23:41:58 -0300

    Update parameterized_adder.v

--- parameterized_adder.v
+++ parameterized_adder.v
@@ -1,10 +1,11 @@
-module add_w
-#(parameter width = 4)
+module add_w #(parameter width = 4)
+	
 	(input wire [width-1:0] a, b,
 	input cin,
-    output [width-1:0]  sum,
+        output [width-1:0]  sum,
 	output cout
 	);
+	
 assign {cout, sum} = a+b+cin;
 
 endmodule 
